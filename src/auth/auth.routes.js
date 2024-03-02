fd4ce80a08cc7f69d480953880b28cab431640f2
lquiyuch2022226 <lquiyuch-2022226@kinal.edu.gt>
2024-03-02 13:08:07 -0600

    Se corrigió las rutas de auth

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,41 +1,17 @@
-import bcryptjs from 'bcryptjs';
-import Usuario from '../user/user.model.js'
-import { generarJWT } from '../helpers/generate-jwt.js'; 
+import { Router } from "express";
+import { check } from "express-validator";
 
-export const login = async (req, res) => {
-    const { correo, password } = req.body;
+import { login } from "./auth.controller.js";
+import { validarCampos } from "../middlewares/validar-campos.js";
 
-  try {
-    const usuario = await Usuario.findOne({ correo });
+const router = Router()
 
-    if (!usuario) {
-      return res.status(400).json({
-        msg: "Incorrect credentials, this email isn't exists in database",
-      });
-    }
-    if (!usuario.estado) {
-      return res.status(400).json({
-        msg: "This user don't exists in database",
-      });
-    }
-    const validPassword = bcryptjs.compareSync(password, usuario.password);
-    if (!validPassword) {
-      return res.status(400).json({
-        msg: "Incorrect password",
-      });
-    }
-    const token = await generarJWT( usuario.id);
+router.post(
+    '/login',
+    [
+        check('correo', "This isn't a valid email").isEmail(),
+        check('password', "The password is required").not().isEmpty(),
+        validarCampos,
+    ], login);
 
-    res.status(200).json({
-      msg: 'You are login',
-      usuario,
-      token
-    });
-
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      msg: "Talks to admin",
-    });
-  }
-}
+export default router
