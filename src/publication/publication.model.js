3bc4073d10512e3196eb17a4777879d8bf57abf6
lquiyuch2022226 <lquiyuch-2022226@kinal.edu.gt>
2024-03-02 16:52:51 -0600

    Se crea el modelo de publicaciones y se crean sus archivos restantes

---
+++ src/publication/publication.model.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const PublicationSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'The title is required']
+    },
+    category: {
+        type: String,
+        required: [true, 'The category is required']
+    },
+    text: {
+        type: String,
+        required: [true, 'The text is required']
+    },
+    idUser: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+});
+
+PublicationSchema.methods.toJSON = function(){
+    const {__v, _id, ...publication} = this.toObject();
+    publication.pid = _id;
+    return publication;
+}
+
+export default mongoose.model('Publication', PublicationSchema)
